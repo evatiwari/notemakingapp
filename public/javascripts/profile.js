6da3587dc84710b61c30b25d0a20fc00d6581e3f
Eva Tiwari <eva.tiwari@gmail.com>
2019-09-15 16:28:04 +0530

    Made website dynamic using a little jquery

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,16 +1,51 @@
-const newNote= document.querySelector("#note");
+/*const newNote= document.querySelector("#note");
       newNote.addEventListener('click',function(){
       var newDiv= document.createElement('div');
       newDiv.className= 'newnote';
       const grid= document.querySelector('.grid');
       grid.appendChild(newDiv);
-      });
+      });*/
 
-      /*var create= document.querySelector('#create');
+      var create= document.querySelector('#note');
     create.addEventListener('click',()=>{
-      var note=document.getElementById('txt').value;
+      //var note=document.getElementById('textarea').value;
+      var note= $('#textarea').val();
       var newDiv= document.createElement('div');
       newDiv.className= 'newnote';
-      newDiv.appendChild(button)
-      $('.newnote').html("<center><p>"+note+"</p><br><button class='delete'>Delete</button><br><button class='edit'>Edit</button>")
-    })*/
+      newDiv.id=Math.floor((Math.random()*100)+1);
+      $(newDiv).html("<center><p>"+note+"</p><br><button class='delete'>Delete</button><br><button class='edit'>Edit</button>");
+      $('.grid').append(newDiv);
+      var del= document.querySelector('.delete');
+      del.addEventListener('click',()=>{
+        $(`#${newDiv.id}`).remove();
+      })
+    });
+
+
+   /* $(document).ready(function () {
+      $('#newnote form').submit(function (e) {
+          e.preventDefault();
+          var request = $.ajax({
+              type: "post",
+              
+              data: {
+                  json: JSON.stringify({
+                      text: $('#newnote').find('textarea').val(),
+                      id: Math.floor((Math.random()*100)+1)
+                  })
+              },
+              dataType: 'json'
+          });
+  
+          request.done(function (note) {
+              $('#notes ').append('<div class=note id='+note.id+'>' + note.text + '<br><div class="edit"><a href="#">Edit</a></div><br><div class="delete">Delete</div></div>');
+              $('#newnote').find('textarea').val('');
+          });
+      });
+  
+      $("#notes").on("click", ".edit", function (e) {
+          e.preventDefault();
+          var id = $(this).parents().first().attr("data-id");
+          alert("You are editing the record with id: " + id);
+      });
+  });*/
